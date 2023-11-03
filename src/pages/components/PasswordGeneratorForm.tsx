b272bb21dd74b49b8cdececa99addc1ecc88b329
NataliyaChe <happylibrarygeek@gmail.com>
2023-11-03 23:45:38 +0200

    handleCheckbox fixed

--- src/pages/components/PasswordGeneratorForm.tsx
+++ src/pages/components/PasswordGeneratorForm.tsx
@@ -1,12 +1,13 @@
 import { Box, Typography, Slider, FormGroup, FormControlLabel, Checkbox, TextField} from "@mui/material"
 import Button from "./UI/Button"
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function PasswordGeneratorForm() {
     const marksValue = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
     const marks = marksValue.map(markItem => ({value: markItem, label: markItem}))
     const [passwordLength, setPasswordLength] = useState(6)
     const [buttonCondition, setButtonCondition] = useState(true)
+    const [checkboxIndex, setCheckboxIndex] = useState(0)
     
     const options = [
         {
@@ -34,23 +35,42 @@ export default function PasswordGeneratorForm() {
             checked: false
         },
     ]
-        
+       
+    const [checkedState, setCheckedState] = useState(
+        new Array(options.length).fill(false)
+    )
+
     function handleSlider(event: Event, newValue: number | number[]) {
         setPasswordLength(newValue as number)    
     }
 
     function generatePassword() {
         console.log('click');
-        
+        options.forEach(item => {
+            if(item.checked) {
+                item.value
+            }
+        })
     }
 
     function handleCheckbox(event: React.BaseSyntheticEvent) {
-        setButtonCondition(!event.target.checked)
+        setCheckboxIndex(Number(event.target.id) - 1)
         options.forEach(item => {
             if(item.id === event.target.id) {
                 item.checked = event.target.checked
             }
         })
+        const updateCheckedState = checkedState.map((item, index) => {
+            if(index === (Number(event.target.id) - 1)) {
+                console.log('if', !item);
+                !item
+            } else {
+                console.log('else', item);
+                item
+            }
+        })
+        console.log('updateCheckedState', updateCheckedState);
+        
     }
 
     return (
@@ -82,15 +102,15 @@ export default function PasswordGeneratorForm() {
                     sx={{mb: 5}}
                 />
                 <FormGroup sx={{mb: 3}}>
-                    {options.map(item => 
+                    {options.map(({id, label}) => 
                         <FormControlLabel 
-                        key={item.id}
+                        key={id}
                         control={
                             <Checkbox 
-                                id={item.id}
+                                id={id}
                                 onChange={handleCheckbox}
                             />} 
-                        label={item.label}
+                        label={label}
                     />
                         )}
                 </FormGroup>
